33aa8bdb8b7ed4a09edc727ad62f4c11dacb360e
ddejrufit <rkd555777@naver.com>
2025-02-05 17:22:57 +0900

    array데이터 정렬버튼

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천','맛집추천','파이썬독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','맛집추천','파이썬독학'])
 
   let [추천수, 개추] = useState(0);
 
@@ -19,8 +19,21 @@ function App() {
     <div className='black-nav'>
     <h4>ReactBlog</h4>
     </div>
+
+      <button onClick={()=>{
+        let abc = [...글제목]
+        abc.sort()
+        글제목변경(abc)
+      }}>가나다순정렬</button>
+
+      <button onClick={()=>{
+
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
     <div className='list'>
-    <h4>{글제목[0]} <span onClick={()=>{ 개추(추천수 + 1) }}>👍</span> {추천수} </h4><span onClick={()=>{ b(글제목 = ['여자 코트 추천','맛집추천','파이썬독학']) }}>❓</span>
+    <h4>{글제목[0]} <span onClick={()=>{ 개추(추천수 + 1) }}>👍</span> {추천수} </h4>
     <p>2월 17일 발행</p>
     </div>
     <div className='list'>
