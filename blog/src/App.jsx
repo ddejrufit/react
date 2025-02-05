7caeb2a41b2703d2d31a06b5c04f863e6a8c8767
ddejrufit <rkd555777@naver.com>
2025-02-05 17:03:15 +0900

    이벤트 핸들러

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코드 추천','맛집추천','파이썬독학'])
+  let [글제목, b] = useState(['남자 코트 추천','맛집추천','파이썬독학'])
+
+  let [추천수, 개추] = useState(0);
 
   // state = 자주변경되는 html 자동으로 재랜더링 해줘서
 
 
-  
 
 
   return (
@@ -18,13 +19,18 @@ function App() {
     <div className='black-nav'>
     <h4>ReactBlog</h4>
     </div>
-    <div className='list'></div>
-    <h4>{글제목[0]}</h4>
-    <p>2월 17일 발행</p><hr />
+    <div className='list'>
+    <h4>{글제목[0]} <span onClick={()=>{ 개추(추천수 + 1) }}>👍</span> {추천수} </h4><span onClick={()=>{ b(글제목 = ['여자 코트 추천','맛집추천','파이썬독학']) }}>❓</span>
+    <p>2월 17일 발행</p>
+    </div>
+    <div className='list'>
     <h4>{글제목[1]}</h4>
-    <p>2월 17일 발행</p><hr />
+    <p>2월 17일 발행</p>
+    </div>
+    <div className='list'>
     <h4>{글제목[2]}</h4>
-    <p>2월 17일 발행</p><hr />
+    <p>2월 17일 발행</p>
+    </div>
     
   </div>
   );
