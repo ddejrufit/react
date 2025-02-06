8c37fa2d45309e414bbeec872b6e8e0a9257e5e4
ddejrufit <rkd555777@naver.com>
2025-02-06 10:37:45 +0900

    반복문 map

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','맛집추천','파이썬독학'])
 
-  let [추천수, 개추] = useState(0);
+  let [추천수, 개추] = useState([0,0,0]);
+
   let [modal, setModal] = useState(0);
   // state = 자주변경되는 html 자동으로 재랜더링 해줘서
 
-
-
+  
 
   return (
   <div className='App'>
@@ -27,24 +27,41 @@ function App() {
       }}>가나다순정렬</button>
 
       <button onClick={()=>{
-
         let copy = [...글제목];
         copy[0] = '여자코트 추천';
         글제목변경(copy);
       }}>글수정</button>
+{/* 
     <div className='list'>
     <h4 onClick={()=>{setModal(!modal)}}>{글제목[0]} <span onClick={()=>{ 개추(추천수 + 1) }}>👍</span> {추천수} </h4>
     <p>2월 17일 발행</p>
     </div>
+
     <div className='list'>
     <h4 onClick={()=>{setModal(!modal)}}>{글제목[1]}</h4>
     <p>2월 17일 발행</p>
     </div>
+
     <div className='list'>
     <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
     <p>2월 17일 발행</p>
-    </div>
+    </div> */}
 
+      {
+        글제목.map(function(a, i){
+          return  (
+            <div className='list' key={i}>
+            <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] } 
+              <span onClick={()=>{
+                let copy = [...추천수]
+                copy[i]++;
+                개추(copy)
+                 }}>👍</span> {추천수[i]} </h4>
+            <p>2월 17일 발행</p>
+             </div>
+          )
+        })
+      }
 
       {
         modal == 1 ? <Modal/> : null
