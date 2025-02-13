f08eb312a2ab801635362caf0c5f8014fb2716f3
ddejrufit <rkd555777@naver.com>
2025-02-13 10:02:22 +0900

    이미지 넣기

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,21 +1,43 @@
 
-import {Button,Container,Nav,Navbar} from 'react-bootstrap';
+import {Container,Nav,Navbar} from 'react-bootstrap';
 import './App.css'
+import bg from './img/bg.png';
 
 function App() {
   return (
-    <>
-        <Navbar bg="dark" data-bs-theme="dark">
+    <div className='bar'>
+        <Navbar bg="dark" data-bs-theme="dark" >
         <Container>
           <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      </>
+    <br />
+      <div className='main-bg' style={{ backgroundImage : 'url(' + bg + ')'}}></div>
+      <div className="container">
+        <div className="row">
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width={"80%"}/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </div>
+        <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width={"80%"}/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </div>
+        <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width={"80%"}/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </div>
+        </div>
+      </div> 
+      
+      </div>
   );
 }
 
