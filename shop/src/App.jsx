095e4dff446c4d6aa2c2f68d77db265395d58233
ddejrufit <rkd555777@naver.com>
2025-02-14 10:11:55 +0900

    import export

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -2,10 +2,16 @@
 import {Container,Nav,Navbar} from 'react-bootstrap';
 import './App.css'
 import bg from './img/bg.png';
+import {useState} from "react";
+import data from './data.js'
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className='bar'>
+      
         <Navbar bg="dark" data-bs-theme="dark" >
         <Container>
           <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
@@ -19,26 +25,37 @@ function App() {
       <div className='main-bg' style={{ backgroundImage : 'url(' + bg + ')'}}></div>
       <div className="container">
         <div className="row">
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width={"80%"}/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
-        <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width={"80%"}/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
-        <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width={"80%"}/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </div>
+      {
+      shoes.map(function(a,i){
+        return(
+        <List shoes={shoes} i={i}/>
+        )
+      })
+      }
         </div>
       </div> 
       
+
+
+    
+
+
       </div>
   );
 }
+
+
+function List(props){
+  return(
+    <div className="col-md-4">
+    <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width={"80%"}/>
+      <h4>{props.shoes[props.i].title}</h4>
+      <p>{props.shoes[props.i].price}</p>
+    </div>
+    
+  
+  )
+}
+
 
 export default App
