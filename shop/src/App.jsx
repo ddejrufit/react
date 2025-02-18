cbb61b6e1cd0ae349f04ca76b97b4ddc4c248e65
ddejrufit <rkd555777@naver.com>
2025-02-18 18:37:37 +0900

    URL 파라미터

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -43,7 +43,8 @@ function App() {
         </div>
       </div> 
       </div>}/>
-      <Route path='/detail' element={<Detail /> }/>
+      <Route path='/detail/:id' element={<Detail  shoes={shoes}/> }/>
+
 
       <Route path='/about' element={<About />}>
         <Route path='member' element={<>멤버임</>}/>
